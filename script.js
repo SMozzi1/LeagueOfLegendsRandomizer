8711d9e1d6d58780b99e182e3741a5c76049bb36
SMozzi <santimozzi2@gmail.com>
2023-06-23 04:08:38 -0300

    images added to the names

--- script.js
+++ script.js
@@ -1,16 +1,77 @@
 let toplaneChampions = ["Aatrox", "Akali", "Camille", "Cassiopeia", "Cho'Gath", "Darius", "DR.Mundo", "Fiora", 
-"Gangplank", "Garen", "Gnar", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", 
+"Gangplank", "Garen", "Gnar", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia",
 "Jax", "Jayce", "K'Sante", "Karma", "Kayle", "Kennen", "Kled", "Lillia", "Lucian", 
 "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nocturne", "Olaf", "Ornn", "Pantheon", 
 "Poppy", "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sett", "Shen", "Singed", 
-"Sion", "Swain", "Sylas", "Tahm Kench", "Teemo", "Tryndamere", "Urgot", "Vayne", "Vladimir", 
-"Volibear", "Wukong", "Yasuo", "Yone", "Yorick", "Zac"]
+"Sion", "Swain", "Sylas", "Talon", "Tahm Kench", "Teemo", "Tryndamere", "Urgot", "Vayne", "Vladimir", 
+"Volibear", "Warwick", "Wukong", "Yasuo", "Yone", "Yorick", "Zed", "Zac", "Ivern"]
+
+let toplaneSquares = [
+  'imagenes/champion-squares-compressed/Aatrox.png', 'imagenes/champion-squares-compressed/Akali.png',
+  'imagenes/champion-squares-compressed/Camille.png', 'imagenes/champion-squares-compressed/Cassiopeia.png',
+  'imagenes/champion-squares-compressed/Chogath.png', 'imagenes/champion-squares-compressed/Darius.png',
+  'imagenes/champion-squares-compressed/DrMundo.png', 'imagenes/champion-squares-compressed/Fiora.png',
+  'imagenes/champion-squares-compressed/Gangplank.png', 'imagenes/champion-squares-compressed/Garen.png',
+  'imagenes/champion-squares-compressed/Gnar.png', 'imagenes/champion-squares-compressed/Gwen.png',
+  'imagenes/champion-squares-compressed/Hecarim.png', 'imagenes/champion-squares-compressed/Heimerdinger.png',
+  'imagenes/champion-squares-compressed/Illaoi.png', 'imagenes/champion-squares-compressed/Irelia.png',
+  'imagenes/champion-squares-compressed/Jax.png', 'imagenes/champion-squares-compressed/Jayce.png',
+  'imagenes/champion-squares-compressed/KSante.png', 'imagenes/champion-squares-compressed/Karma.png',
+  'imagenes/champion-squares-compressed/Kayle.png', 'imagenes/champion-squares-compressed/Kennen.png',
+  'imagenes/champion-squares-compressed/Kled.png', 'imagenes/champion-squares-compressed/Lillia.png',
+  'imagenes/champion-squares-compressed/Lucian.png', 'imagenes/champion-squares-compressed/Malphite.png',
+  'imagenes/champion-squares-compressed/Maokai.png', 'imagenes/champion-squares-compressed/Mordekaiser.png',
+  'imagenes/champion-squares-compressed/Nasus.png', 'imagenes/champion-squares-compressed/Nocturne.png',
+  'imagenes/champion-squares-compressed/Olaf.png', 'imagenes/champion-squares-compressed/Ornn.png',
+  'imagenes/champion-squares-compressed/Pantheon.png', 'imagenes/champion-squares-compressed/Poppy.png',
+  'imagenes/champion-squares-compressed/Quinn.png', 'imagenes/champion-squares-compressed/Renekton.png',
+  'imagenes/champion-squares-compressed/Rengar.png', 'imagenes/champion-squares-compressed/Riven.png',
+  'imagenes/champion-squares-compressed/Rumble.png', 'imagenes/champion-squares-compressed/Ryze.png',
+  'imagenes/champion-squares-compressed/Sett.png', 'imagenes/champion-squares-compressed/Shen.png',
+  'imagenes/champion-squares-compressed/Singed.png', 'imagenes/champion-squares-compressed/Sion.png',
+  'imagenes/champion-squares-compressed/Swain.png', 'imagenes/champion-squares-compressed/Sylas.png', 
+  'imagenes/champion-squares-compressed/Talon.png',
+  'imagenes/champion-squares-compressed/TahmKench.png', 'imagenes/champion-squares-compressed/Teemo.png',
+  'imagenes/champion-squares-compressed/Tryndamere.png',
+  'imagenes/champion-squares-compressed/Urgot.png', 'imagenes/champion-squares-compressed/Vayne.png',
+  'imagenes/champion-squares-compressed/Vladimir.png', 'imagenes/champion-squares-compressed/Volibear.png',
+  'imagenes/champion-squares-compressed/Warwick.png', 'imagenes/champion-squares-compressed/Wukong.png',
+  'imagenes/champion-squares-compressed/Yasuo.png',
+  'imagenes/champion-squares-compressed/Yone.png', 'imagenes/champion-squares-compressed/Yorick.png',
+  'imagenes/champion-squares-compressed/Zed.png', 'imagenes/champion-squares-compressed/Zac.png',
+  'imagenes/champion-squares-compressed/Ivern.png'
+];
 
 let jungleChampions = ['Amumu', 'Bel\'Veth', 'Diana', 'Dr. Mundo', 'Ekko', 'Elise', 'Evelynn', 
 'Fiddlesticks', 'Gragas', 'Graves', 'Hecarim', 'Ivern', 'Jarvan IV', 'Jax', 'Karthus', 'Kayn', 
 'Kha\'Zix', 'Kindred', 'Kled', 'Lee Sin', 'Lillia', 'Master Yi', 'Nidalee', 'Nocturne', 'Nunu', 
 'Olaf', 'Poppy', 'Rammus', 'Rek\'Sai', 'Rengar', 'Sejuani', 'Sett', 'Shaco', 'Shyvana','Skarner',
 'Taliyah','Trundle','Udyr','Vi','Viego','Volibear','Warwick','Wukong','Xin Zhao','Zac'];
+
+let jungleSquares =
+["imagenes/champion-squares-compressed/Amumu.png", "imagenes/champion-squares-compressed/Belveth.png", 
+"imagenes/champion-squares-compressed/Diana.png", "imagenes/champion-squares-compressed/DrMundo.png", 
+"imagenes/champion-squares-compressed/Ekko.png", "imagenes/champion-squares-compressed/Elise.png", 
+"imagenes/champion-squares-compressed/Evelynn.png", "imagenes/champion-squares-compressed/Fiddlesticks.png", 
+"imagenes/champion-squares-compressed/Gragas.png", "imagenes/champion-squares-compressed/Graves.png", 
+"imagenes/champion-squares-compressed/Hecarim.png", "imagenes/champion-squares-compressed/Ivern.png", 
+"imagenes/champion-squares-compressed/JarvanIV.png", "imagenes/champion-squares-compressed/Jax.png", 
+"imagenes/champion-squares-compressed/Karthus.png", "imagenes/champion-squares-compressed/Kayn.png", 
+"imagenes/champion-squares-compressed/Khazix.png", "imagenes/champion-squares-compressed/Kindred.png", 
+"imagenes/champion-squares-compressed/Kled.png", "imagenes/champion-squares-compressed/LeeSin.png", 
+"imagenes/champion-squares-compressed/Lillia.png", "imagenes/champion-squares-compressed/MasterYi.png", 
+"imagenes/champion-squares-compressed/Nidalee.png", "imagenes/champion-squares-compressed/Nocturne.png", 
+"imagenes/champion-squares-compressed/Nunu.png", "imagenes/champion-squares-compressed/Olaf.png", 
+"imagenes/champion-squares-compressed/Poppy.png", "imagenes/champion-squares-compressed/Rammus.png", 
+"imagenes/champion-squares-compressed/RekSai.png","imagenes/champion-squares-compressed/Rengar.png",
+"imagenes/champion-squares-compressed/Sejuani.png","imagenes/champion-squares-compressed/Sett.png",
+"imagenes/champion-squares-compressed/Shaco.png","imagenes/champion-squares-compressed/Shyvana.png",
+"imagenes/champion-squares-compressed/Skarner.png","imagenes/champion-squares-compressed/Taliyah.png",
+"imagenes/champion-squares-compressed/Trundle.png","imagenes/champion-squares-compressed/Udyr.png",
+"imagenes/champion-squares-compressed/Vi.png","imagenes/champion-squares-compressed/Viego.png",
+"imagenes/champion-squares-compressed/Volibear.png","imagenes/champion-squares-compressed/Warwick.png",
+"imagenes/champion-squares-compressed/Wukong.png","imagenes/champion-squares-compressed/XinZhao.png",
+"imagenes/champion-squares-compressed/Zac.png", "imagenes/champion-squares-compressed/Zed.png"];
 
 let midlaneChampions = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Anivia', 'Annie', 'Aurelion Sol', 
 'Azir', 'Camille', 'Cassiopeia', 'Cho\'Gath', 'Corki', 'Diana', 'Ekko', 'Fizz', 'Galio', 'Garen', 
@@ -19,15 +80,156 @@ let midlaneChampions = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Anivia', 'Annie', 
 'Pyke','Qiyana','Renekton','Rumble','Ryze','Swain','Sylas','Syndra','Taliyah','Talon','Twisted Fate',
 'Veigar','Vel\'Koz','Vex','Viktor','Vladimir','Xerath','Yasuo','Yone','Zed','Ziggs','Zilean ','Zoe'];
 
+let midlaneSquares = [
+    'imagenes/champion-squares-compressed/Aatrox.png',
+  'imagenes/champion-squares-compressed/Ahri.png',
+  'imagenes/champion-squares-compressed/Akali.png',
+  'imagenes/champion-squares-compressed/Akshan.png',
+  'imagenes/champion-squares-compressed/Anivia.png',
+  'imagenes/champion-squares-compressed/Annie.png',
+  'imagenes/champion-squares-compressed/Aurelion Sol.png',
+  'imagenes/champion-squares-compressed/Azir.png',
+  'imagenes/champion-squares-compressed/Camille.png',
+  'imagenes/champion-squares-compressed/Cassiopeia.png',
+  'imagenes/champion-squares-compressed/ChoGath.png',
+  'imagenes/champion-squares-compressed/Corki.png',
+  'imagenes/champion-squares-compressed/Diana.png',
+  'imagenes/champion-squares-compressed/Ekko.png',
+  'imagenes/champion-squares-compressed/Fizz.png',
+  'imagenes/champion-squares-compressed/Galio.png',
+  'imagenes/champion-squares-compressed/Garen.png',
+  'imagenes/champion-squares-compressed/Gwen.png',
+  'imagenes/champion-squares-compressed/Heimerdinger.png',
+  'imagenes/champion-squares-compressed/Irelia.png',
+  'imagenes/champion-squares-compressed/Ivern.png',
+  'imagenes/champion-squares-compressed/Jayce.png',
+  'imagenes/champion-squares-compressed/Kassadin.png',
+  'imagenes/champion-squares-compressed/Katarina.png',
+  'imagenes/champion-squares-compressed/LeBlanc.png',
+  'imagenes/champion-squares-compressed/Lillia.png',
+  'imagenes/champion-squares-compressed/Lissandra.png',
+  'imagenes/champion-squares-compressed/Lucian.png',
+  'imagenes/champion-squares-compressed/Lux.png',
+  'imagenes/champion-squares-compressed/Malphite.png',
+  'imagenes/champion-squares-compressed/Malzahar.png',
+  'imagenes/champion-squares-compressed/Neeko.png',
+  'imagenes/champion-squares-compressed/Nocturne.png',
+  'imagenes/champion-squares-compressed/Nunu.png',
+  'imagenes/champion-squares-compressed/Orianna.png',
+  'imagenes/champion-squares-compressed/Pantheon.png',
+  'imagenes/champion-squares-compressed/Pyke.png',
+  'imagenes/champion-squares-compressed/Qiyana.png',
+  'imagenes/champion-squares-compressed/Renekton.png',
+  'imagenes/champion-squares-compressed/Rumble.png',
+  'imagenes/champion-squares-compressed/Ryze.png',
+  'imagenes/champion-squares-compressed/Swain.png',
+  'imagenes/champion-squares-compressed/Sylas.png',
+  'imagenes/champion-squares-compressed/Syndra.png',
+  'imagenes/champion-squares-compressed/Taliyah.png',
+  'imagenes/champion-squares-compressed/Talon.png',
+  'imagenes/champion-squares-compressed/TwistedFate.png',
+  'imagenes/champion-squares-compressed/Veigar.png',
+  'imagenes/champion-squares-compressed/VelKoz.png',
+  'imagenes/champion-squares-compressed/Vex.png',
+  'imagenes/champion-squares-compressed/Viktor.png',
+  'imagenes/champion-squares-compressed/Vladimir.png',
+  'imagenes/champion-squares-compressed/Xerath.png',
+  'imagenes/champion-squares-compressed/Yasuo.png',
+  'imagenes/champion-squares-compressed/Yone.png',
+  'imagenes/champion-squares-compressed/Zed.png',
+  'imagenes/champion-squares-compressed/Ziggs.png',
+  'imagenes/champion-squares-compressed/Zilean.png',
+  'imagenes/champion-squares-compressed/Zoe.png',
+];
+
 let adcChampions = ['Aphelios', 'Ashe', 'Caitlyn', 'Corki', 'Draven', 'Ezreal', 'Graves', 'Jhin', 
 'Jinx', 'Kai\'Sa', 'Kalista', 'Kindred', 'Kog\'Maw', 'Lucian', 'Miss Fortune', 'Samira', 'Senna', 
 'Quinn', 'Sivir', 'Tristana', 'Twitch', 'Varus','Vayne','Xayah'];
+
+let adcSquares = [
+    'imagenes/champion-squares-compressed/Aphelios.png',
+    'imagenes/champion-squares-compressed/Ashe.png',
+    'imagenes/champion-squares-compressed/Caitlyn.png',
+    'imagenes/champion-squares-compressed/Corki.png',
+    'imagenes/champion-squares-compressed/Draven.png',
+    'imagenes/champion-squares-compressed/Ezreal.png',
+    'imagenes/champion-squares-compressed/Graves.png',
+    'imagenes/champion-squares-compressed/Jhin.png',
+    'imagenes/champion-squares-compressed/Jinx.png',
+    'imagenes/champion-squares-compressed/KaiSa.png',
+    'imagenes/champion-squares-compressed/Kalista.png',
+    'imagenes/champion-squares-compressed/Kindred.png',
+    'imagenes/champion-squares-compressed/KogMaw.png',
+    'imagenes/champion-squares-compressed/Lucian.png',
+    'imagenes/champion-squares-compressed/MissFortune.png',
+    'imagenes/champion-squares-compressed/Samira.png',
+    'imagenes/champion-squares-compressed/Senna.png',
+    'imagenes/champion-squares-compressed/Quinn.png',
+    'imagenes/champion-squares-compressed/Sivir.png',
+    'imagenes/champion-squares-compressed/Tristana.png',
+    'imagenes/champion-squares-compressed/Twitch.png',
+    'imagenes/champion-squares-compressed/Varus.png',
+    'imagenes/champion-squares-compressed/Vayne.png',
+    'imagenes/champion-squares-compressed/Xayah.png',
+  ];
 
 let supportChampions = ['Alistar', 'Amumu', 'Anivia', 'Annie', 'Ashe', 'Bard', 'Blitzcrank', 
 'Brand', 'Braum', 'Fiddlesticks', 'Galio', 'Heimerdinger', 'Ivern', 'Janna', 'Karma', 'Leona', 
 'Lulu', 'Lux', 'Malphite','Maokai','Milio','Morgana','Nami','Nautilus','Pantheon','Poppy','Pyke',
 'Rakan','Rell','Renata Glasc','Senna','Seraphine','Sett','Shaco','Shen','Sona','Soraka','Swain',
 'Syndra','Tahm Kench','Taric','Thresh','Vel\'Koz','Xerath','Yuumi','Zac','Zilean ','Zoe ','Zyra'];
+
+let supportSquares = [
+    'imagenes/champion-squares-compressed/Alistar.png',
+    'imagenes/champion-squares-compressed/Amumu.png',
+    'imagenes/champion-squares-compressed/Anivia.png',
+    'imagenes/champion-squares-compressed/Annie.png',
+    'imagenes/champion-squares-compressed/Ashe.png',
+    'imagenes/champion-squares-compressed/Bard.png',
+    'imagenes/champion-squares-compressed/Blitzcrank.png',
+    'imagenes/champion-squares-compressed/Brand.png',
+    'imagenes/champion-squares-compressed/Braum.png',
+    'imagenes/champion-squares-compressed/Fiddlesticks.png',
+    'imagenes/champion-squares-compressed/Galio.png',
+    'imagenes/champion-squares-compressed/Heimerdinger.png',
+    'imagenes/champion-squares-compressed/Ivern.png',
+    'imagenes/champion-squares-compressed/Janna.png',
+    'imagenes/champion-squares-compressed/Karma.png',
+    'imagenes/champion-squares-compressed/Leona.png',
+    'imagenes/champion-squares-compressed/Lulu.png',
+    'imagenes/champion-squares-compressed/Lux.png',
+    'imagenes/champion-squares-compressed/Malphite.png',
+    'imagenes/champion-squares-compressed/Maokai.png',
+    'imagenes/champion-squares-compressed/Milio.png',
+    'imagenes/champion-squares-compressed/Morgana.png',
+    'imagenes/champion-squares-compressed/Nami.png',
+    'imagenes/champion-squares-compressed/Nautilus.png',
+    'imagenes/champion-squares-compressed/Pantheon.png',
+    'imagenes/champion-squares-compressed/Poppy.png',
+    'imagenes/champion-squares-compressed/Pyke.png',
+    'imagenes/champion-squares-compressed/Rakan.png',
+    'imagenes/champion-squares-compressed/Rell.png',
+    'imagenes/champion-squares-compressed/RenataGlasc.png',
+    'imagenes/champion-squares-compressed/Senna.png',
+    'imagenes/champion-squares-compressed/Seraphine.png',
+    'imagenes/champion-squares-compressed/Sett.png',
+    'imagenes/champion-squares-compressed/Shaco.png',
+    'imagenes/champion-squares-compressed/Shen.png',
+    'imagenes/champion-squares-compressed/Sona.png',
+    'imagenes/champion-squares-compressed/Soraka.png',
+    'imagenes/champion-squares-compressed/Swain.png',
+    'imagenes/champion-squares-compressed/Syndra.png',
+    'imagenes/champion-squares-compressed/Tahm Kench.png',
+    'imagenes/champion-squares-compressed/Taric.png',
+    'imagenes/champion-squares-compressed/Thresh.png',
+    'imagenes/champion-squares-compressed/VelKoz.png',
+    'imagenes/champion-squares-compressed/Xerath.png',
+    'imagenes/champion-squares-compressed/Yuumi.png',
+    'imagenes/champion-squares-compressed/Zac.png',
+    'imagenes/champion-squares-compressed/Zilean.png',
+    'imagenes/champion-squares-compressed/Zoe.png',
+    'imagenes/champion-squares-compressed/Zyra.png',
+  ];
 
 
 function detectorDeOpcion() {
@@ -36,29 +238,45 @@ function detectorDeOpcion() {
     return value;
 }
 
-function randomizarPersonaje(){
-    let linea = detectorDeOpcion()
-    let randomChamp = [];
-    let result = [];
-    switch (linea){
-    case "top":  randomChamp = Math.floor(Math.random() * toplaneChampions.length);
-         result =  toplaneChampions[randomChamp];
-    break;
-    case "jg":  randomChamp = Math.floor(Math.random() * jungleChampions.length);
-     result =  jungleChampions[randomChamp];
-    break;
-    case "mid":  randomChamp = Math.floor(Math.random() * midlaneChampions.length);
-     result =  midlaneChampions[randomChamp];
-     break;
-     case "adc": randomChamp = Math.floor(Math.random() * adcChampions.length);
-     result =  adcChampions[randomChamp];
-     break;
-     case "supp": randomChamp = Math.floor(Math.random() * supportChampions.length);
-     result =  supportChampions[randomChamp];
-     break;
+function randomizarPersonaje() {
+    const linea = detectorDeOpcion();
+    let randomChamp = "";
+    let result = "";
+    let imgSrc = "";
+    
+    switch (linea) {
+      case "top":
+        randomChamp = Math.floor(Math.random() * toplaneChampions.length);
+        result = toplaneChampions[randomChamp];
+        imgSrc = toplaneSquares[randomChamp];
+        break;
+      case "jg":
+        randomChamp = Math.floor(Math.random() * jungleChampions.length);
+        result = jungleChampions[randomChamp];
+        imgSrc = jungleSquares[randomChamp];
+        break;
+      case "mid":
+        randomChamp = Math.floor(Math.random() * midlaneChampions.length);
+        result = midlaneChampions[randomChamp];
+        imgSrc = midlaneSquares[randomChamp];
+        break;
+      case "adc":
+        randomChamp = Math.floor(Math.random() * adcChampions.length);
+        result = adcChampions[randomChamp];
+        imgSrc = adcSquares[randomChamp];
+        break;
+      case "supp":
+        randomChamp = Math.floor(Math.random() * supportChampions.length);
+        result = supportChampions[randomChamp];
+        imgSrc = supportSquares[randomChamp];
+        break;
     }
-    return result;
-}
-document.querySelector("#randomicer").addEventListener("click", function() {
-    document.querySelector("#result").textContent = randomizarPersonaje();
-});
+  
+    return { champion: result, imageSrc: imgSrc };
+  }
+  
+  document.querySelector("#randomicer").addEventListener("click", function() {
+    const { champion, imageSrc } = randomizarPersonaje();
+    document.querySelector("#result").textContent = champion;
+    document.querySelector("#championSquare").src = imageSrc;
+  });
